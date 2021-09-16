10003d91539d9360ddc032a9fc5a69a25577b73f
rjmsas <Rahul.Maurya@connectwise.com>
2021-09-16 18:53:00 -0400

    added file start-jenkins-if-offline-or-not-running-with-port.sh

---
+++ start-jenkins-if-offline-or-not-running-with-port.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#d=$(docker ps -a | grep -i $1 | awk '{print $9}')
+d=$(docker ps -a | grep -i $1 | grep -o 8080 | uniq)
+if [[ -z $d  ]]
+then
+        docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker rm -f {}
+        docker run -d -p 8080:8080 -v /opt/jenkins:/var/jenkins_home  jenkins/jenkins
+        echo "docker reinstalled"
+else
+        echo "docker service is already up and running"
+fi
