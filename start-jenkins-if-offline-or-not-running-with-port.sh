bcaee1f5bbe0ccc07ada456ad5bef9bb64589b11
rjmsas <Rahul.Maurya@connectwise.com>
2021-09-17 16:49:06 -0400

    changed the file

--- start-jenkins-if-offline-or-not-running-with-port.sh
+++ start-jenkins-if-offline-or-not-running-with-port.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #d=$(docker ps -a | grep -i $1 | awk '{print $9}')
-d=$(docker ps -a | grep -i $1 | grep -o 8080 | uniq)
+#d=$(docker ps -a | grep -i $1 | grep -o 8080 | uniq)
+d=$(netstat -tulpn | grep 8080)
 if [[ -z $d  ]]
 then
         docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker rm -f {}
